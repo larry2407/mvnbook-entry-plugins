74bc24bbbe02861eb71e12c9e5fd17e40a4afb2b
greaumaxime <greaumaxime@gmail.com>
2011-05-24 15:04:52 +0000

    mise en forme client

--- chapitre5/mvnbook-entry-webservice/trunk/mvnbook-command/src/main/java/com/mgreau/mvnbook/MvnBookCli.java
+++ chapitre5/mvnbook-entry-webservice/trunk/mvnbook-command/src/main/java/com/mgreau/mvnbook/MvnBookCli.java
@@ -11,56 +11,94 @@ import org.slf4j.LoggerFactory;
 import com.mgreau.mvnbook.webservice.client.ClientWS;
 
 /**
- * @author mgreau
+ * Client pour faire un appel de WS avec une librairie autonome.
  * 
+ * @author Maxime Gréau - SRE
+ *
  */
 public class MvnBookCli {
 
+	/** Logger */
 	public static final Logger log = LoggerFactory.getLogger(MvnBookCli.class);
+
+	/** URL du serveur où se trouve le webservice */
+	private String url = "http://localhost:8080";
+
+	/** WebService à appeler */
+	private String ws;
+
+	/** Path du POM à ajouter */
+	private String file = null;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		log.info("Execution du CLI en mode commande -------");
+		log.info("------- Execution du CLI en mode commande -------");
 		MvnBookCli cli = new MvnBookCli();
 		if (null == args || args.length == 0)
 			log.error(cli.getArgsAvailable());
 		else {
-			cli.showArgs(args);
-			ClientWS cliWS = new ClientWS();
-			try {
-				String res = cliWS.addPlugin(new File("test"));
-				log.info("ok call done." + res);
-			} catch (Exception e) {
-				log.error("erreur d'appel WS", e);
+			if (cli.manageArgs(args)) {
+				ClientWS cliWS = new ClientWS();
+				try {
+					log.info("- appel du WS addPlugin...");
+					String response = cliWS.addPlugin(new File(cli.file));
+					boolean resultat = (response != null && response.startsWith("SUCCESS")?true:false);
+					log.info("");
+					log.info("-------------------");
+					log.info("AJOUT DU PLUGIN " + (resultat?"OK":"KO"));
+					if (resultat)
+						log.info("Plugin : " + response.split(":")[1] + ":" + response.split(":")[2]);
+					log.info("-------------------");
+				} catch (Exception e) {
+					log.error("erreur d'appel WS", e);
+				}
 			}
 		}
-
-		log.info("-----------Fin du processus.");
+		log.info("----------- Fin du processus -----------");
 
 	}
 
-	public void showArgs(String[] args) {
+	/**
+	 * Gestion des arguments.
+	 * 
+	 * @param args
+	 * @return
+	 */
+	public boolean manageArgs(String[] args) {
+		boolean argsOk = true;
 		for (String arg : args) {
-			if (!arg.startsWith("-D")) {
-				log.error("Les arguments doivent être préciser par la syntaxe -Darg=valeur");
-				break;
+			if (!arg.startsWith("url=") && !arg.startsWith("file=") && !arg.startsWith("ws=")) {
+				if (!arg.startsWith("-D")) {
+					log.error("L'argument " + arg + "  n'est pas autorisé.");
+					argsOk = false;
+					break;
+				}
+			} else {
+				if (arg.startsWith("url")) {
+					url = arg.split("=")[1];
+				} else if (arg.startsWith("file")) {
+					file = arg.split("=")[1];
+				} else if (arg.startsWith("ws")) {
+					ws = arg.split("=")[1];
+				}
 			}
-			log.info(arg.substring(1));
-
 		}
+		return argsOk;
 	}
 
+	/**
+	 * Affiche la liste des arguments diponibles.
+	 * @return la liste des args autorisés
+	 */
 	public String getArgsAvailable() {
 		StringBuffer args = new StringBuffer();
 		args.append(
-				"-Durl=http://<ip>:<port> : adresse du serveur où est déployé le Web Service")
+				"url=http://<ip>:<port> : adresse du serveur où est déployé le Web Service")
 				.append('\n');
-		args.append("-Dws=<name> : web service à appeler").append('\n');
-		args.append("-Dfile=<path> : chemin d'acces au fichier POM").append(
-				'\n');
+		args.append("ws=<name> : web service à appeler").append('\n');
+		args.append("file=<path> : chemin d'acces au fichier POM").append('\n');
 		return args.toString();
 	}
-
 }
